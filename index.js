cfc2bada52af2636e2b49672a9dc1332fef3c2fe
Huy Le <huy.le1@ptnglobalcorp.com>
2024-10-05 23:02:52 +0700

    feat: add proxy router

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get("/", (req, res) => {
     res.send('SERVER ON')
 })
 
+app.get('/proxy', async (req, res) => {
+    const targetUrl = req.query.url
+    try {
+        const response = await axios.get(targetUrl, {
+            headers: {
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+        res.json(response.data) // Send the response from the target URL back to the client
+    } catch (error) {
+        console.error('Error fetching data from target URL:', error)
+        res.status(500).send('Error fetching data')
+    }
+})
+
 // ZingMp3Router
 const ZingMp3Router = require("./src/routes/ZingRouter")
 app.use("/api", cors({ origin: '*' }), ZingMp3Router)
