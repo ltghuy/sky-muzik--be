a2e9839e5b199954f9b07893da298fe2e4f84262
ltghuy <letrangiahuy0507@gmail.com>
2024-10-05 23:42:12 +0700

    fix: update proxy router

--- index.js
+++ index.js
@@ -10,21 +10,35 @@ app.get("/", (req, res) => {
     res.send('SERVER ON')
 })
 
-app.get('/proxy', async (req, res) => {
-    const targetUrl = req.query.url
+// Proxy Route
+app.get('/api/proxy', async (req, res) => {
+    const targetUrl = req.query.url;
+
+    // Check if the target URL is provided
+    if (!targetUrl) {
+        return res.status(400).send('No target URL specified');
+    }
+
     try {
         const response = await axios.get(targetUrl, {
             headers: {
                 'Accept': 'application/json',
                 'X-Requested-With': 'XMLHttpRequest'
             }
-        })
-        res.json(response.data) // Send the response from the target URL back to the client
+        });
+
+        // Check if the response status is 200
+        if (response.status === 200) {
+            res.json(response.data); // Send the response from the target URL back to the client
+        } else {
+            console.error('Error fetching data:', response.status);
+            res.status(response.status).send('Error fetching data from target URL');
+        }
     } catch (error) {
-        console.error('Error fetching data from target URL:', error)
-        res.status(500).send('Error fetching data')
+        console.error('Error fetching data from target URL:', error);
+        res.status(500).send('Error fetching data');
     }
-})
+});
 
 // ZingMp3Router
 const ZingMp3Router = require("./src/routes/ZingRouter")
